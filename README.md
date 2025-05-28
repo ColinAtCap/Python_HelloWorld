50e8288cd864d9c212e5cdcd1c043fc0167eb339
ColinAtCap <colin.neville@capgemini.com>
2025-05-28 10:08:11 +0100

    Streamlit and Readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Python_Hello_World
 First test repo for VSC and Python
+
+Now with added Streamlit
---
+++ Streamlit.py
@@ -0,0 +1,7 @@
+# Streamlit HelloWorld
+
+import streamlit as st
+
+st.write("Hello World!")
+st.write("I am Streamlit")
+
